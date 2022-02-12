1b77dc86b63abc96df8214a2f32cdff73efbc7d1
Tatiana Kikalova <t_kikalova@bk.ru>
2022-02-12 22:29:24 +0100

    New branch for pull request.

--- lesson_1_intro_knn/homework/dataload.py
+++
@@ -1,5 +0,0 @@
-import pickle
-from sklearn.datasets import fetch_openml
-X, y = fetch_openml(name="Fashion-MNIST", return_X_y=True, as_frame=False)
-pickle.dump(X, open("X.txt", "wb"))
-pickle.dump(y, open("y.txt", "wb"))
